bc96780cc3b3797dffc7bd3fe92f834596d6ecfd
İdil Zeynep Alemdar <cydoniza@192.168.1.17>
2024-06-11 17:24:49 +0300

    add publisher

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +16,13 @@ public class BootstrapData implements CommandLineRunner {
 
     private final BookRepository bookRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+        PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -52,5 +58,14 @@ public class BootstrapData implements CommandLineRunner {
 
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
+
+        Publisher oreilly = new Publisher();
+        oreilly.setPublisherName("O'Reilly");
+        oreilly.setState("Colorado");
+
+        publisherRepository.save(oreilly);
+
+        System.out.println("Publisher count: " + publisherRepository.count());
+
     }
 }
