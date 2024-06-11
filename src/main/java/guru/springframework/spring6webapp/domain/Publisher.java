006e6d1c7335852380d8a3828c0a857f54974ac3
İdil Zeynep Alemdar <cydoniza@192.168.1.17>
2024-06-11 19:23:03 +0300

    publisher relations

--- src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.Set;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -20,6 +23,17 @@ public class Publisher {
     private String city;
     private String state;
     private String zipCode;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 
     public Long getId() {
         return id;
